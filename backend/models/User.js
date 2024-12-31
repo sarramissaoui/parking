4c397d24f51741e8bc6ba1de9d2ca9ecfde4686a
Sarra Missaoui <missaouisarra18@gmail.com>
2024-12-31 16:42:43 +0100

    Initial commit

---
+++ backend/models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    isAdmin: { type: Boolean, default: false, },
+});
+
+userSchema.methods.comparePassword = function(candidatePassword) {
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+        if (err) return reject(err);
+        resolve(isMatch);
+      });
+    });
+  };
+
+userSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 12);
+    next();
+});
+
+module.exports = mongoose.model('User', userSchema);
